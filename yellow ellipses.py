a94336ddc5b566023119c2459b8872b13ee452c2
TIN-slayer <dimka_dmit@mail.ru>
2019-12-06 12:13:18 +0300

    first

---
+++ yellow ellipses.py
@@ -0,0 +1,39 @@
+import sys
+from random import randint
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import Qt
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('elip.ui', self)
+        self.f = False
+        size = self.size()
+        self.x, self.y = size.width(), size.height()
+        self.pushButton.clicked.connect(self.elipse)
+
+    def elipse(self):
+        self.f = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.f:
+            color = QColor(Qt.yellow)
+            self.qp = QPainter()
+            x, y = randint(0, self.x - 1), randint(0, self.y - 1)
+            max_cd = randint(1, min(self.x - x, self.y - y))
+            self.qp.begin(self)
+            self.qp.setPen(color)
+            self.qp.drawEllipse(x, y, max_cd, max_cd)
+            self.qp.end()
+            self.f = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
